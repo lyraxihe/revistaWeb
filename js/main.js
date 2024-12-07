2ae54e07c756eb37d25bd2182f03700fa22e95e3
Raime6 <xcanalshernaez@gmail.com>
2024-12-07 23:41:15 +0100

    Footer implemented
    With flex containers, a form and a button that creates a popup.

--- js/main.js
+++ js/main.js
@@ -4,18 +4,69 @@ $(document).ready(function()
     /* ========================================================= */
     // HEADER
 
+    /* --------------------------------------------------------- */
     // menuItem hover
     let menuItems = $("section .menu_flex a");
     menuItems.hover(function()
     {
         $(this).toggleClass("hover_menuItem");
     })
+    /* --------------------------------------------------------- */
 
+    /* --------------------------------------------------------- */
     // suscribeButton hover
     let suscribeButton = $(".suscribeButton");
     suscribeButton.hover(function()
     {
-        suscribeButton.toggleClass("hover_Button");
+        suscribeButton.toggleClass("hover_suscribeButton");
     })
+    /* --------------------------------------------------------- */
+
+    /* --------------------------------------------------------- */
+    // Social Media Logos
+    let socialMediaLogos = $("footer .socialMedia a img");
+    socialMediaLogos.hover(function()
+    {
+        $(this).toggleClass("hover_socialMediaLogo");
+    })
+    /* --------------------------------------------------------- */
+
+    /* --------------------------------------------------------- */
+    // Form's fields
+    $(".contactUs_form input, .contactUs_form textarea").focus(function() {
+        $(this).addClass("input_focus");
+    });
+    
+    $(".contactUs_form input, .contactUs_form textarea").blur(function() {
+        $(this).removeClass("input_focus");
+    });
+    /* --------------------------------------------------------- */
+
+    /* --------------------------------------------------------- */
+    // sendMessageButton hover
+    let sendMessageButton = $(".sendMessageButton");
+    sendMessageButton.hover(function()
+    {
+        sendMessageButton.toggleClass("hover_sendMessageButton");
+    })
+
+    sendMessageButton.click(function()
+    {
+        let nameText = $(".nameText_input").val().trim();
+        let emailText = $(".emailText_input").val().trim();
+        let messageText = $(".messageText_input").val().trim();
+
+        if (nameText === "" || emailText === "" || messageText === "")
+        {
+            alert("ERROR.\nPlease, fill all the fields before sending the message.");
+        }
+        else
+        {
+            alert("Message sent successfully");
+        }
+
+        $(".contactUs_form")[0].reset();
+    })
+    /* --------------------------------------------------------- */
     /* ========================================================= */
 });
