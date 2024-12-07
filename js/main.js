d78368c5e0c7ee55de3ed1136ad06b306c439bf0
Raime6 <xcanalshernaez@gmail.com>
2024-12-07 06:34:24 +0100

    Header implemented + Body background
    - Header needs responsive behaviour

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 
+$(document).ready(function()
+{
+    /* ========================================================= */
+    // HEADER
 
+    // menuItem hover
+    let menuItems = $("section .menu_flex a");
+    menuItems.hover(function()
+    {
+        $(this).toggleClass("hover_menuItem");
+    })
+
+    // suscribeButton hover
+    let suscribeButton = $(".suscribeButton");
+    suscribeButton.hover(function()
+    {
+        suscribeButton.toggleClass("hover_Button");
+    })
+    /* ========================================================= */
+});
